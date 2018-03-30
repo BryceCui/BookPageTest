dfe89415055a6800c8870b353d529f18f833d2c6
cuipengyu <cuipengyusoul@gmail.com>
2018-03-30 17:05:42 +0800

    2018.03.30

--- app/src/main/java/com/cuipengyu/bookpagetest/MainActivity.java
+++ app/src/main/java/com/cuipengyu/bookpagetest/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private String bookId;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
